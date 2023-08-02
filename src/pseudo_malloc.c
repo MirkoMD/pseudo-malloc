53321ec5ba6a75441721dd06046045e98fea9713
Mirko Magistrado Dacara <101113216+MirkoMD@users.noreply.github.com>
2023-08-02 16:58:22 +0200

    Fix pseudo_malloc_init

--- src/pseudo_malloc.c
+++ src/pseudo_malloc.c
@@ -2,19 +2,22 @@
 #include "buddy_allocator.h"
 #include <sys/mman.h>
 
-#define BUFFER_SIZE 102400
 #define BUDDY_LEVELS 9
-#define MEMORY_SIZE (1024 * 1024)
-#define MIN_BUCKET_SIZE (MEMORY_SIZE >> (BUDDY_LEVELS))
+#define MEMORY_SIZE (1 << 20)
+#define MIN_BUCKET_SIZE (MEMORY_SIZE >> (BUDDY_LEVELS - 1))
+#define NUM_BUDDIES ((1 << BUDDY_LEVELS) - 1)
+#define BITSET_SIZE ((NUM_BUDDIES + 7) << 3)
 
-char buffer[BUFFER_SIZE]; // 100 Kb buffer to handle memory should be enough
 char memory[MEMORY_SIZE];
+uint8_t buffer[BITSET_SIZE];
 
 BuddyAllocator alloc;
+BitMap bitmap;
 
 void pseudo_malloc_init()
 {
-    BuddyAllocator_init(&alloc, BUDDY_LEVELS, buffer, BUFFER_SIZE, memory, MIN_BUCKET_SIZE);
+    BitMap_init(&bitmap, NUM_BUDDIES, buffer);
+    BuddyAllocator_init(&alloc, &bitmap, memory, BUDDY_LEVELS, MIN_BUCKET_SIZE);
 }
 
 void *pseudo_malloc(size_t size)
