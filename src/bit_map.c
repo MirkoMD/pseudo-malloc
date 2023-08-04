9b622d8f718ac8e2e31febfcd41785ba089663d9
Mirko Magistrado Dacara <101113216+MirkoMD@users.noreply.github.com>
2023-08-04 22:32:38 +0200

    Fix bitmap

--- src/bit_map.c
+++ src/bit_map.c
@@ -24,7 +24,7 @@ void BitMap_setBit(BitMap *bit_map, int bit_num, int status)
 	// get byte
 	int byte_num = bit_num >> 3;
 	assert(byte_num < bit_map->buffer_size);
-	int bit_in_byte = byte_num & 0x03;
+	int bit_in_byte = bit_num & 0x03;
 	if (status)
 	{
 		bit_map->buffer[byte_num] |= (1 << bit_in_byte);
@@ -40,6 +40,6 @@ int BitMap_bit(const BitMap *bit_map, int bit_num)
 {
 	int byte_num = bit_num >> 3;
 	assert(byte_num < bit_map->buffer_size);
-	int bit_in_byte = byte_num & 0x03;
+	int bit_in_byte = bit_num & 0x03;
 	return (bit_map->buffer[byte_num] & (1 << bit_in_byte)) != 0;
 }
